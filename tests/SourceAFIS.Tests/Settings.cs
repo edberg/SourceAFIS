9f7ece7f8d80cff91e517ed2414cb11c375840ec
Rasmus Oudal Edberg <re@cryosinternational.com>
2016-10-06 16:30:14 +0200

    Converted release 1.7 to .net core

---
+++ tests/SourceAFIS.Tests/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Drawing;
+
+namespace SourceAFIS.Tests
+{
+    static class Settings
+    {
+        public static string RootFolder = Path.GetFullPath(Path.Combine(".."));
+        public static string DataPath = Path.Combine(RootFolder, "Data");
+        public static string IsoTemplatePath = Path.Combine(DataPath, "IsoTemplates");
+        public static string ImagePath = Path.Combine(DataPath, "Images");
+
+        public static string SomeFingerprintPath = Path.Combine(ImagePath, "101_1.tif");
+        public static string MatchingFingerprintPath = Path.Combine(ImagePath, "101_2.tif");
+        public static string NonMatchingFingerprintPath = Path.Combine(ImagePath, "102_1.tif");
+
+        public static Bitmap SomeFingerprint = new Bitmap(SomeFingerprintPath);
+        public static Bitmap MatchingFingerprint = new Bitmap(MatchingFingerprintPath);
+        public static Bitmap NonMatchingFingerprint = new Bitmap(NonMatchingFingerprintPath);
+    }
+}
